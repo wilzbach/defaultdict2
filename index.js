9de00cea62cc3965bfc71ab155d5a71c6ab5b831
Sebastian Wilzbach <seb@wilzba.ch>
2016-02-05 05:34:14 +0200

    check whether --harmony-proxies is enabled

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-require("harmonize")(["harmony-proxies"]);
+try{
+    // see whether proxy is available
+    var handler = {
+        get: function(target, name){}
+    };
+    Proxy.create({}, handler);
+}catch(e){
+    // hack to enable the harmony-proxies flag by default
+    require("harmonize")(["harmony-proxies"]);
+}
 var defaultdict = require('./src/index.js');
 module.exports = defaultdict;
